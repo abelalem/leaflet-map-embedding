01391a82129a383efbd0bb60acf9ea03f5039bce
Alemayehu Ewunetu <AEwunetu@excellerentsolutions.com>
2023-05-16 20:53:57 +0300

    Started working on map embedding in react.

--- packages/react/map-embedding/src/app/app.tsx
+++ packages/react/map-embedding/src/app/app.tsx
@@ -1,12 +1,26 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
+import * as L from 'leaflet';
 
 export function App() {
+  const locations: {
+    locationId: number
+    locationName: string,
+    gpsCoordinate: {
+      latitiude: string,
+      longitude: string
+    }
+  }[] = [];
+
+  const selectedLocations: {
+    locationId: number,
+    latitude: number,
+    longitude: number,
+    marker: L.Marker
+  }[] = [];
   return (
     <div>
-      <NxWelcome title="react-map-embedding" />
+      Welcome to react map embedding
     </div>
   );
 }
