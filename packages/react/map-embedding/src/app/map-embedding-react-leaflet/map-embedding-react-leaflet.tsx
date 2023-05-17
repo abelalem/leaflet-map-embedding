2dce4134937592b30f4af6db50c9e0956ce75956
Alemayehu Ewunetu <AEwunetu@excellerentsolutions.com>
2023-05-17 19:30:32 +0300

    Start testing react-leaflet

--- packages/react/map-embedding/src/app/map-embedding-react-leaflet/map-embedding-react-leaflet.tsx
+++ packages/react/map-embedding/src/app/map-embedding-react-leaflet/map-embedding-react-leaflet.tsx
@@ -1,12 +1,29 @@
 import styles from './map-embedding-react-leaflet.module.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 /* eslint-disable-next-line */
 export interface MapEmbeddingReactLeafletProps {}
 
 export function MapEmbeddingReactLeaflet(props: MapEmbeddingReactLeafletProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to MapEmbeddingReactLeaflet!</h1>
+    <div>
+      <h1>Map Embedding React-Leaflet</h1>
+      <div style={{
+        width: '100%',
+        height: '300px'
+      }}>
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+          <TileLayer
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[51.505, -0.09]} >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </div>
   );
 }
